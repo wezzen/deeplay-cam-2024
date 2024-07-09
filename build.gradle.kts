6deea0b6f78eaba52768d3cff15218fad50c89e5
wezen <dimariqqq@gmail.com>
2024-07-09 17:09:14 +0600

    jacoco and errorprone

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    id("java")
+    java
+    id("net.ltgt.errorprone") version "4.0.1"
+    jacoco
 }
 
 group = "io.deeplay.camp"
@@ -9,11 +11,27 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    errorprone("com.google.errorprone:error_prone_core:2.28.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(false)
+    }
 }
