fb8660a62d4a010f3538c40ebbc48e6972aea91d
wezen <dimariqqq@gmail.com>
2024-07-09 17:12:40 +0600

    add and multiply methods

--- src/main/java/io/deeplay/camp/Main.java
+++ src/main/java/io/deeplay/camp/Main.java
@@ -4,4 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static int add(final int a, final int b) {
+        return a + b;
+    }
+
+    public static int multiply(final int a, final int b) {
+        return a * b;
+    }
 }
