2855bf22521895f67d8a8cf945ff1d8ed32679ba
Mmmeerkat <tateross5@gmail.com>
2024-07-11 18:03:36 +0700

    INT-14 multi-module project gradle INT-27 change .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,35 +3,37 @@ plugins {
     id("net.ltgt.errorprone") version "4.0.1"
     jacoco
 }
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "jacoco")
+    apply(plugin = "net.ltgt.errorprone")
 
-group = "io.deeplay.camp"
-version = "1.0-SNAPSHOT"
+    repositories {
+        mavenCentral()
+    }
 
-repositories {
-    mavenCentral()
-}
+    java {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-}
+    dependencies {
+        testImplementation(platform("org.junit:junit-bom:5.10.0"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+        errorprone("com.google.errorprone:error_prone_core:2.28.0")
+    }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    errorprone("com.google.errorprone:error_prone_core:2.28.0")
-}
+    tasks.test {
+        useJUnitPlatform()
+        finalizedBy(tasks.jacocoTestReport)
+    }
 
-tasks.test {
-    useJUnitPlatform()
-    finalizedBy(tasks.jacocoTestReport)
-}
-
-tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-    reports {
-        xml.required.set(true)
-        csv.required.set(false)
-        html.required.set(false)
+    tasks.jacocoTestReport {
+        dependsOn(tasks.test)
+        reports {
+            xml.required.set(true)
+            csv.required.set(false)
+            html.required.set(false)
+        }
     }
 }
