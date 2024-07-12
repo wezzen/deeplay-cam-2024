6e3393df269e6402446f5178ea603e609c5a6a62
BakinII <bakin_ii@mail.ru>
2024-07-12 18:31:19 +0700

    test: добавил тест проверяющий симетричность относительно побочной диагонали матрицы

--- src/test/java/io/deeplay/camp/engine/entities/FieldTest.java
+++ src/test/java/io/deeplay/camp/engine/entities/FieldTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FieldTest {
 
@@ -24,5 +25,16 @@ class FieldTest {
         assertNotNull(field.getBoard(), "Field board not be null");
         assertEquals(5, field.getBoard().length, "Field board size = 5x5");
         assertEquals(5, field.getBoard()[0].length, "Field board size = 5x5");
+    }
+
+    @Test
+    void testFieldSymmetry() {
+        for (int i = 0; i < field.getSize(); i++) {
+            for (int j = 0; j < i; j++) {
+                if(field.getBoard()[i][j].planet!=null){
+                    assertNotNull(field.getBoard()[field.getSize()-1-i][field.getSize()-1-j].planet);
+                }
+            }
+        }
     }
 }
