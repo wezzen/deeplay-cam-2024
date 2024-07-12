cabcf1fc2b61053a54731abe7ceacb7079571da4
BakinII <bakin_ii@mail.ru>
2024-07-12 12:44:02 +0700

    fix: изменил уровни доступа к final полям и поправил форматирование

--- src/main/java/io/deeplay/camp/engine/entities/Cell.java
+++ src/main/java/io/deeplay/camp/engine/entities/Cell.java
@@ -1,35 +1,23 @@
 package io.deeplay.camp.engine.entities;
 
 /**
-*Класс клетки поля
-* */
+ * Класс клетки поля
+ */
 public class Cell {
-    private final int x;
-    private final int y;
+    public final int x;
+    public final int y;
+    public final Planet planet;
 
-    private final Planet planet;
-
-    public Cell(final int x,final int y, Planet planet) {
-        this.planet= planet;
+    public Cell(final int x, final int y, final Planet planet) {
+        this.planet = planet;
         this.x = x;
         this.y = y;
     }
-    public Cell(final int x,final int y) {
+
+    public Cell(final int x, final int y) {
         this.x = x;
         this.y = y;
-        this.planet=null;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public Planet getPlanet() {
-        return planet;
+        this.planet = null;
     }
 
 }
