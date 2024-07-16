9e821a0d9eac1b4ccbe55deb27d0e1f71cceb995
Andrey Shelyagin <ShelyaginAndrey@yandex.ru>
2024-07-16 12:15:21 +0600

    Изменил интерфейс, указал типы для методов и вхлдные значения. Пояснил принадлежность перечислений, написанных ранее, не могу ответить на вопрос, есть ли смысл их оставить

--- src/main/java/io/deeplay/camp/engine/entities/domain/GameTypes.java
+++ src/main/java/io/deeplay/camp/engine/entities/domain/GameTypes.java
@@ -1,5 +1,8 @@
 package io.deeplay.camp.engine.entities.domain;
 
+/**
+ * Разные типы игры, основанные на видах пользователя - участника
+ */
 public enum GameTypes {
     BotVsBot,
     HumanVsBot,
