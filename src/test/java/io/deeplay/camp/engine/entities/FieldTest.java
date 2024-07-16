09b703431a9ca693868aaf602a8fc1bb302fb5d1
Andrey Shelyagin <ShelyaginAndrey@yandex.ru>
2024-07-16 13:54:51 +0600

    Учел замечания по ПР, сделал класс Game aka контроллер, но не закончил с его логикой разбираться. Внес в клетку поле fleet

--- src/test/java/io/deeplay/camp/engine/entities/FieldTest.java
+++ src/test/java/io/deeplay/camp/engine/entities/FieldTest.java
@@ -27,4 +27,13 @@ class FieldTest {
         assertEquals(5, field.getBoard().length, "Field board size = 5x5");
         assertEquals(5, field.getBoard()[0].length, "Field board size = 5x5");
     }
+
+    @Test
+    void testFieldSymmetry() {
+        for (int i = 0; i < field.getSize(); i++) {
+            for (int j = 0; j < i; j++) {
+                assertEquals(field.getBoard()[i][j].planet, field.getBoard()[j][i].planet);
+            }
+        }
+    }
 }
