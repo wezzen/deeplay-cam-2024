a927589613dbb6be4e25f3cfe7fa62ff8fe2f76e
BakinII <bakin_ii@mail.ru>
2024-07-18 12:23:01 +0700

    test: добавил тесты на новый метод

--- src/test/java/io/deeplay/camp/engine/entities/PlanetTest.java
+++ src/test/java/io/deeplay/camp/engine/entities/PlanetTest.java
@@ -1,14 +1,29 @@
 package io.deeplay.camp.engine.entities;
 
 import io.deeplay.camp.engine.entities.Planet;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlanetTest {
+
+    Planet planet;
+
+    @BeforeEach
+    void setUp() {
+        planet = new Planet(10);
+    }
+
     @Test
     void testPlanetCreationWithState() {
-        Planet planet = new Planet(10);
         assertEquals(10, planet.points, "Planet points = 10");
+    }
+
+    @Test
+    void testCaptureOnPlanet() {
+        assertFalse(planet.isCaptured());
+        planet.setOwner(new Player(1, "test"));
+        assertTrue(planet.isCaptured());
     }
 }
