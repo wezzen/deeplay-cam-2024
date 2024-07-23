0990db42086ece940dba7839523fd9fe95c054a3
Andrey Shelyagin <ShelyaginAndrey@yandex.ru>
2024-07-23 17:57:38 +0800

    fix: Модификации с классом Field (int-103)

--- game/src/test/java/io/deeplay/camp/game/entities/FieldTest.java
+++ game/src/test/java/io/deeplay/camp/game/entities/FieldTest.java
@@ -1,19 +1,27 @@
 package io.deeplay.camp.game.entities;
 
 import io.deeplay.camp.game.entites.Field;
+import io.deeplay.camp.game.entites.Planet;
+import io.deeplay.camp.game.entites.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FieldTest {
 
     private Field field;
+    private Player player1;
+    private Player player2;
 
     @BeforeEach
     void setUp() {
         field = new Field(5);
+        player1 = new Player(0, "player1");
+        player2 = new Player(1, "player2");
+        for (Planet planet : field.getPlanets()) {
+            planet.setOwner(player1); // initially setting all planets to player1
+        }
     }
 
     @Test
@@ -35,5 +43,25 @@ class FieldTest {
                 assertEquals(field.getBoard()[i][j].planet, field.getBoard()[j][i].planet);
             }
         }
+    }
+
+    @Test
+    void testIsGameOver() {
+        assertTrue(field.isGameOver(), "All planets should belong to player1, so the game should be over");
+
+        // Change the owner of one planet
+        field.getPlanets().get(0).setOwner(player2);
+
+        assertFalse(field.isGameOver(), "Not all planets belong to the same player, so the game should not be over");
+    }
+
+    @Test
+    void testIsWinner() {
+        assertEquals(player1, field.isWinner(), "Player1 should be the winner");
+
+        // Change the owner of one planet
+        field.getPlanets().get(0).setOwner(player2);
+
+        assertNull(field.isWinner(), "There should be no winner as not all planets belong to the same player");
     }
 }
