245d3736b1e11a3b515ed2ff3d3f79a54aba4d19
BakinII <bakin_ii@mail.ru>
2024-07-23 20:07:05 +0700

    fix: почистил тесты

--- game/src/test/java/io/deeplay/camp/game/domain/GameTypesTest.java
+++ game/src/test/java/io/deeplay/camp/game/domain/GameTypesTest.java
@@ -8,9 +8,7 @@ class GameTypesTest {
     @Test
     public void testEnumValues() {
         GameTypes[] expectedValues = {
-                GameTypes.BotVsBot,
                 GameTypes.HumanVsBot,
-                GameTypes.HumansHuman
         };
 
         GameTypes[] actualValues = GameTypes.values();
@@ -20,22 +18,16 @@ class GameTypesTest {
 
     @Test
     public void testEnumValueOf() {
-        assertEquals(GameTypes.BotVsBot, GameTypes.valueOf("BotVsBot"));
         assertEquals(GameTypes.HumanVsBot, GameTypes.valueOf("HumanVsBot"));
-        assertEquals(GameTypes.HumansHuman, GameTypes.valueOf("HumansHuman"));
     }
 
     @Test
     public void testEnumNames() {
-        assertEquals("BotVsBot", GameTypes.BotVsBot.name());
         assertEquals("HumanVsBot", GameTypes.HumanVsBot.name());
-        assertEquals("HumansHuman", GameTypes.HumansHuman.name());
     }
 
     @Test
     public void testEnumOrdinal() {
-        assertEquals(0, GameTypes.BotVsBot.ordinal());
         assertEquals(1, GameTypes.HumanVsBot.ordinal());
-        assertEquals(2, GameTypes.HumansHuman.ordinal());
     }
 }
