7e57a5291ca5452743ba9e62c0058e7b5176a694
Mmmeerkat <tateross5@gmail.com>
2024-07-23 11:49:18 +0700

    int-23 fix: delete test witch check "fleet == null" in endPosition

--- src/test/java/io/deeplay/camp/engine/entities/utils/MovementTest.java
+++ src/test/java/io/deeplay/camp/engine/entities/utils/MovementTest.java
@@ -23,14 +23,6 @@ class MovementTest {
         Move move = new Move(new Cell(9, 9, new Fleet(new ArrayList<>(), new Cell(9, 9))), new Cell(10, 10), Move.MoveType.ORDINARY);
         assertFalse(Movement.isValidMove(move, field));
     }
-
-    @Test
-    void testInvalidAvailableMove() {
-        Field field = new Field(10);
-        Move move = new Move(new Cell(5, 5, new Fleet(new ArrayList<>(), new Cell(5, 5))), new Cell(6, 6, new Fleet(new ArrayList<>(), new Cell(6, 6))), Move.MoveType.ORDINARY);
-        assertFalse(Movement.isValidMove(move, field));
-    }
-
     @Test
     void testInvalidMoveType() {
         Field field = new Field(10);
