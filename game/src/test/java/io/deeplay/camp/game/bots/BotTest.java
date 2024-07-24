d53960d1ab40bbe166551e5d5910b285c224c26d
Andrey Shelyagin <ShelyaginAndrey@yandex.ru>
2024-07-25 00:52:22 +0800

    fix: добавил тесты, пока что для покрытия

--- game/src/test/java/io/deeplay/camp/game/bots/BotTest.java
+++ game/src/test/java/io/deeplay/camp/game/bots/BotTest.java
@@ -1,87 +1,18 @@
 package io.deeplay.camp.game.bots;
 
 import io.deeplay.camp.game.domain.GameTypes;
-import io.deeplay.camp.game.entites.*;
+import io.deeplay.camp.game.entites.Answer;
+import io.deeplay.camp.game.entites.Cell;
+import io.deeplay.camp.game.entites.Field;
+import io.deeplay.camp.game.entites.Move;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BotTest {
 
     private Field field;
-    private Bot testBot;
-    private Player player;
-    private Fleet fleet;
-    private Ship ship;
-
-    /*@BeforeEach
-    public void setUp() {
-        Ship ship = new Ship(Ship.ShipType.BASIC, fleet);
-        List<Ship> shipList= new ArrayList<Ship>();
-        field = new Field(8); // Например, создаём поле размером 8x8
-        fleet = new Fleet(new Cell(0, 0), player);
-        player = new Player(1, "TestPlayer");
-        testBot = new RandomBot.Factory(player).createBot(field);
-    }*/
-
-    @BeforeEach
-    public void setUp() {
-        field = new Field(8); // Например, создаём поле размером 8x8
-        player = new Player(1, "TestPlayer");
-        Cell position = field.getBoard()[0][0];
-        fleet = new Fleet(position, player);
-        fleet = new Fleet(new Cell(0, 0), player); // Инициализируем флот
-        ship = new Ship(Ship.ShipType.BASIC, fleet);
-        testBot = new RandomBot.Factory(player).createBot(field);
-    }
-
-    @Test
-    public void testGetAnswer() {
-        Answer answer = testBot.getAnswer(field);
-        assertNotNull(answer, "Answer should not be null");
-        Move move = answer.getMove();
-        assertNotNull(move, "Move should not be null");
-        assertNotNull(move.startPosition(), "Start position of the move should not be null");
-        assertNotNull(move.endPosition(), "End position of the move should not be null");
-        assertEquals(Move.MoveType.ORDINARY, move.moveType(), "Move type should be ORDINARY");
-    }
-
-    @Test
-    public void testStartGameSession() {
-        assertDoesNotThrow(() -> testBot.startGameSession("testGameId", GameTypes.BotVsBot), "startGameSession should not throw any exception");
-    }
-
-    @Test
-    public void testConnectingPlayer() {
-        assertDoesNotThrow(() -> testBot.connectingPlayer("Player1"), "connectingPlayer should not throw any exception");
-    }
-
-    @Test
-    public void testGameStarted() {
-        assertDoesNotThrow(() -> testBot.gameStarted(field), "gameStarted should not throw any exception");
-    }
-
-    @Test
-    public void testGetPlayerAction() {
-        Move move = new Move(new Cell(0, 0), new Cell(1, 1), Move.MoveType.ORDINARY);
-        assertDoesNotThrow(() -> testBot.getPlayerAction(move, "Player1"), "getPlayerAction should not throw any exception");
-    }
-
-    @Test
-    public void testGameEnded() {
-        assertDoesNotThrow(() -> testBot.gameEnded("Player1"), "gameEnded should not throw any exception");
-    }
-
-    @Test
-    public void testEndGameSession() {
-        assertDoesNotThrow(() -> testBot.endGameSession(), "endGameSession should not throw any exception");
-    }
-    /*private Field field;
     private Bot testBot;
 
     @BeforeEach
@@ -126,5 +57,5 @@ public class BotTest {
     @Test
     public void testEndGameSession() {
         assertDoesNotThrow(() -> testBot.endGameSession());
-    }*/
+    }
 }
--- game/src/test/java/io/deeplay/camp/game/bots/RandomBotTest.java
+++ game/src/test/java/io/deeplay/camp/game/bots/RandomBotTest.java
@@ -1,6 +1,8 @@
 package io.deeplay.camp.game.bots;
 
-import io.deeplay.camp.game.entites.*;
+import io.deeplay.camp.game.entites.Field;
+import io.deeplay.camp.game.entites.Move;
+import io.deeplay.camp.game.entites.Cell;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,63 +11,19 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RandomBotTest {
 
     private Field field;
-    private Player player;
-    private RandomBot bot;
+    private RandomBot randomBot;
 
     @BeforeEach
-    void setUp() {
-        int size = 5;
-        field = new Field(size);
-        player = new Player(1, "TestPlayer");
-        Cell position = field.getBoard()[0][0];
-        // Инициализация флота и добавление его в ячейку
-        Fleet fleet = new Fleet(position, player);
-        Ship ship = new Ship(Ship.ShipType.BASIC, fleet);
-
-        bot = new RandomBot(field, player);
+    public void setUp() {
+        field = new Field(8); // Например, создаём поле размером 8x8
+        randomBot = new RandomBot.Factory().createBot(field);
     }
 
     @Test
-    void testGetMove() {
-        Move move = bot.getMove();
-        assertNotNull(move, "Move should not be null");
-        assertNotNull(move.startPosition(), "Start position should not be null");
-        assertNotNull(move.endPosition(), "End position should not be null");
-
-        Cell startCell = move.startPosition();
-        Cell endCell = move.endPosition();
-        assertTrue(startCell.x >= 0 && startCell.x < field.getBoard().length, "Start cell row should be within board range");
-        assertTrue(startCell.y >= 0 && startCell.y < field.getBoard()[0].length, "Start cell column should be within board range");
-        assertTrue(endCell.x >= 0 && endCell.x < field.getBoard().length, "End cell row should be within board range");
-        assertTrue(endCell.y >= 0 && endCell.y < field.getBoard()[0].length, "End cell column should be within board range");
-    }
-
-    @Test
-    void testMoveType() {
-        Move move = bot.getMove();
-        assertEquals(Move.MoveType.ORDINARY, move.moveType(), "Move type should be ORDINARY");
-    }
-
-    @Test
-    void testMakeMove() {
-        int initialFleetCount = player.getFleetList().size();
-        Move move = bot.getMove();
-        // Проверяем, что флот перемещен корректно
-        if (move.startPosition().getFleet() != null) {
-            assertNotNull(move.endPosition().getFleet(), "End position should have a fleet after the move");
-            assertNull(move.startPosition().getFleet(), "Start position should be empty after the move");
-        }
-        // Проверяем изменение списка флотов игрока, если флот уничтожен или перемещен
-        int finalFleetCount = player.getFleetList().size();
-        assertTrue(finalFleetCount <= initialFleetCount, "Final fleet count should not exceed the initial count");
-    }
-
-    @Test
-    void testGetRandomCell() {
-        Cell[][] board = field.getBoard();
-        Cell randomCell = bot.getRandomCell(board);
-        assertNotNull(randomCell, "Random cell should not be null");
-        assertTrue(randomCell.x >= 0 && randomCell.x < board.length, "Random cell row should be within board range");
-        assertTrue(randomCell.y >= 0 && randomCell.y < board[0].length, "Random cell column should be within board range");
+    public void testGetAction() {
+        Move expectedMove = new Move(new Cell(0, 0), new Cell(1, 1), Move.MoveType.ORDINARY);
+        Move move = randomBot.getAction();
+        assertNotNull(move);
+        assertEquals(expectedMove, move);
     }
 }
