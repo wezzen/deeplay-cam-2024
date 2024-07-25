337bc9bdccb596d3d16797c247453dff519858df
BakinII <bakin_ii@mail.ru>
2024-07-26 02:27:26 +0700

    feat: использую другой логгер

--- game/src/main/java/io/deeplay/camp/game/service/GameLogger.java
+++ game/src/main/java/io/deeplay/camp/game/service/GameLogger.java
@@ -1,72 +1,66 @@
 package io.deeplay.camp.game.service;
 
 
-import com.sun.tools.javac.Main;
 import io.deeplay.camp.game.domain.GalaxyListener;
 import io.deeplay.camp.game.domain.GameTypes;
 import io.deeplay.camp.game.entites.Field;
 import io.deeplay.camp.game.entites.Move;
+import org.slf4j.LoggerFactory;
 
-import java.io.FileInputStream;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+
+import org.slf4j.Logger;
 
 
 public class GameLogger implements GalaxyListener {
     static Logger LOGGER;
     private Field field = null;
 
-    static {
-        try (FileInputStream ins = new FileInputStream("log.config")) {
-            LogManager.getLogManager().readConfiguration(ins);
-            LOGGER = Logger.getLogger(GalaxyListener.class.getName());
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+    private static final Logger logger = LoggerFactory.getLogger(
+            GalaxyListener.class);
+
+    private static final String FILENAME = "/logs/log.txt";
 
     @Override
     public void startGameSession(String gameId, GameTypes gameType) {
-        LOGGER.log(Level.INFO, "Начало игровой сессии " + gameId);
-        LOGGER.log(Level.INFO, "Игра в формате " + gameType.toString());
+        logger.info("Начало игровой сессии {}", gameId);
+        logger.info("Игра в формате {}", gameType.toString());
+
     }
 
     @Override
     public void connectingPlayer(String waitingPlayerName) {
-        LOGGER.log(Level.INFO, "Подключился игрок " + waitingPlayerName);
+        logger.info("Подключился игрок {}", waitingPlayerName);
     }
 
     @Override
     public void gameStarted(Field field) {
         this.field = field;
-        LOGGER.log(Level.INFO, "Начало игры ");
+        logger.info("Начало игры ");
         String fieldString = ConvertorFieldToString.convertFieldToString(field);
-        LOGGER.log(Level.INFO, "\n" + fieldString);
+        logger.info("\n{}", fieldString);
     }
 
     @Override
     public void getPlayerAction(Move move, String playerName) {
-        LOGGER.log(Level.INFO, "Сходил игрок " + playerName);
+        logger.info("Сходил игрок {}", playerName);
         if (field != null) {
             field.updateField();//todo: жду пока дпопишется логика ходов
             String fieldString = ConvertorFieldToString.convertFieldToString(field);
-            LOGGER.log(Level.INFO, "\n" + fieldString);
+            logger.info("\n{}", fieldString);
         } else {
-            LOGGER.log(Level.WARNING, "ПУСТОЕ ПОЛЕ");
+            logger.error("ПУСТОЕ ПОЛЕ");
         }
     }
 
     @Override
     public void gameEnded(String winner) {
-        LOGGER.log(Level.INFO, "Игра закончилась. Выиграл игрок:" + winner);
-
+        logger.info("Игра закончилась. Выиграл игрок:{}", winner);
     }
 
 
     @Override
     public void endGameSession() {
-        LOGGER.log(Level.INFO, "Конец игровой сессии");
+        logger.info("Конец игровой сессии");
     }
 
 
