a5fd26b3dff4b67643fd7c385f4c0c80b4168df9
Andrey Shelyagin <ShelyaginAndrey@yandex.ru>
2024-07-26 13:22:07 +0800

    fix: Написал тесты

--- game/src/main/java/io/deeplay/camp/game/bots/RandomBot.java
+++ game/src/main/java/io/deeplay/camp/game/bots/RandomBot.java
@@ -3,20 +3,54 @@ package io.deeplay.camp.game.bots;
 import io.deeplay.camp.game.entites.Cell;
 import io.deeplay.camp.game.entites.Field;
 import io.deeplay.camp.game.entites.Move;
+import io.deeplay.camp.game.entites.Player;
+
+import java.util.List;
+import java.util.Random;
 
 public class RandomBot extends Bot {
-    protected RandomBot(Field field) {
+    private final Player player;
+    private final Field field;
+    protected RandomBot(Field field, Player player) {
         super(field);
+        this.player = player;
+        this.field = field;
     }
     @Override
-    protected Move getAction() {
-        return new Move(new Cell(0, 0), new Cell(1, 1), Move.MoveType.ORDINARY);
+    protected Move getMove() {
+        Cell[][] board = field.getBoard();
+        Cell startCell = getRandomCell(board);
+        Cell endCell = getRandomCell(board);
+        Move.MoveType moveType = Move.MoveType.ORDINARY; // или другая логика определения типа хода
+
+        Move move = new Move(startCell, endCell, moveType);
+        move.makeMove(player);
+        return move;
     }
 
-    public static class Factory extends Bot.BotFactory {
+    /**
+     * Метод для получения случайной клетки из игрового поля.
+     *
+     * @param board двумерный массив клеток игрового поля.
+     * @return случайная клетка.
+     */
+    protected Cell getRandomCell(Cell[][] board) {
+        Random random = new Random();
+        int row = random.nextInt(board.length);
+        int col = random.nextInt(board[row].length);
+        return board[row][col];
+    }
+
+    public static class Factory extends BotFactory {
+        private final Player player;
+
+        public Factory(Player player) {
+            this.player = player;
+        }
+
         @Override
         public RandomBot createBot(final Field field) {
-            return new RandomBot(field);
+            return new RandomBot(field, player);
         }
     }
 }
--- game/src/test/java/io/deeplay/camp/game/bots/RandomBotTest.java
+++ game/src/test/java/io/deeplay/camp/game/bots/RandomBotTest.java
@@ -1,8 +1,6 @@
 package io.deeplay.camp.game.bots;
 
-import io.deeplay.camp.game.entites.Field;
-import io.deeplay.camp.game.entites.Move;
-import io.deeplay.camp.game.entites.Cell;
+import io.deeplay.camp.game.entites.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +9,77 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RandomBotTest {
 
     private Field field;
+    private Player player;
+    private RandomBot bot;
+
+    @BeforeEach
+    void setUp() {
+        int size = 5;
+        field = new Field(size);
+        player = new Player(1, "TestPlayer");
+        bot = new RandomBot(field, player);
+
+        // Заполнение поля клетками для тестов
+        Cell[][] board = new Cell[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                board[i][j] = new Cell(i, j);
+            }
+        }
+        field.setBoard(board);
+
+        Fleet fleet = new Fleet(new Cell(0, 0), player);
+        Ship ship = new Ship(Ship.ShipType.BASIC, fleet);
+    }
+
+    @Test
+    void testGetMove() {
+        Move move = bot.getMove();
+        assertNotNull(move, "Move should not be null");
+        assertNotNull(move.startPosition(), "Start position should not be null");
+        assertNotNull(move.endPosition(), "End position should not be null");
+
+        Cell startCell = move.startPosition();
+        Cell endCell = move.endPosition();
+        assertTrue(startCell.x >= 0 && startCell.x < field.getBoard().length, "Start cell row should be within board range");
+        assertTrue(startCell.y >= 0 && startCell.y < field.getBoard()[0].length, "Start cell column should be within board range");
+        assertTrue(endCell.x >= 0 && endCell.x < field.getBoard().length, "End cell row should be within board range");
+        assertTrue(endCell.y >= 0 && endCell.y < field.getBoard()[0].length, "End cell column should be within board range");
+    }
+
+    @Test
+    void testMoveType() {
+        Move move = bot.getMove();
+        assertEquals(Move.MoveType.ORDINARY, move.moveType(), "Move type should be ORDINARY");
+    }
+
+    @Test
+    void testMakeMove() {
+        int initialFleetCount = player.getFleetList().size();
+        Move move = bot.getMove();
+
+        // Проверяем, что флот перемещен корректно
+        if (move.startPosition().getFleet() != null) {
+            assertNotNull(move.endPosition().getFleet(), "End position should have a fleet after the move");
+            assertNull(move.startPosition().getFleet(), "Start position should be empty after the move");
+        }
+
+        // Проверяем изменение списка флотов игрока, если флот уничтожен или перемещен
+        int finalFleetCount = player.getFleetList().size();
+        assertTrue(finalFleetCount <= initialFleetCount, "Final fleet count should not exceed the initial count");
+    }
+
+    @Test
+    void testGetRandomCell() {
+        Cell[][] board = field.getBoard();
+        Cell randomCell = bot.getRandomCell(board);
+        assertNotNull(randomCell, "Random cell should not be null");
+        assertTrue(randomCell.x >= 0 && randomCell.x < board.length, "Random cell row should be within board range");
+        assertTrue(randomCell.y >= 0 && randomCell.y < board[0].length, "Random cell column should be within board range");
+    }
+
+
+    /*private Field field;
     private RandomBot randomBot;
 
     @BeforeEach
@@ -25,5 +94,5 @@ public class RandomBotTest {
         Move move = randomBot.getAction();
         assertNotNull(move);
         assertEquals(expectedMove, move);
-    }
+    }*/
 }
