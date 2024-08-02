9e62df47eb65268177e1359cbcd3779dd61eea57
Mmmeerkat <tateross5@gmail.com>
2024-08-02 12:51:09 +0700

    fix: fix GalaxyEntityTest

--- game/src/test/java/io/deeplay/camp/game/entites/GalaxyEntityTest.java
+++ game/src/test/java/io/deeplay/camp/game/entites/GalaxyEntityTest.java
@@ -17,8 +17,12 @@ class GalaxyEntityTest {
         GalaxyEntity entity1 = new GalaxyEntity();
         GalaxyEntity entity2 = new GalaxyEntity();
         GalaxyEntity entity3 = new GalaxyEntity();
+        int hashCode1 = entity1.hashCode();
+        int hashCode2 = entity2.hashCode();
         assertNotEquals(entity1, entity2);
         assertNotEquals(entity1, entity3);
         assertNotEquals(entity2, entity3);
+        assertNotEquals(hashCode1, hashCode2);
+
     }
 }
