d0a681a63e2e80eace31bd9271bfd9ec55ebde06
BakinII <bakin_ii@mail.ru>
2024-08-08 04:36:45 +0700

    feat: small fix

--- game/src/main/java/io/deeplay/camp/game/bots/Bot.java
+++ game/src/main/java/io/deeplay/camp/game/bots/Bot.java
@@ -83,7 +83,7 @@ public abstract class Bot implements PlayerInterface {
         final Move move;
         if (move_.moveType() != Move.MoveType.SKIP) {
             Cell[][] b = game.getField().getBoard();
-            move = new Move(b[move_.startPosition().x][move_.startPosition().y], b[move_.endPosition().x][move_.endPosition().y], move_.moveType, move_.cost());
+            move = new Move(b[move_.startPosition().x][move_.startPosition().y], b[move_.endPosition().x][move_.endPosition().y], move_.moveType(), move_.cost());
         } else {
             move = move_;
         }
