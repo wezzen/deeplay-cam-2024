ee504f335715d26b338986d991329294d0439266
BakinII <bakin_ii@mail.ru>
2024-08-09 02:05:04 +0700

    feat: добавил в логер Game

--- game/src/test/java/io/deeplay/camp/game/utils/GameLoggerTest.java
+++ game/src/test/java/io/deeplay/camp/game/utils/GameLoggerTest.java
@@ -4,24 +4,34 @@ import io.deeplay.camp.game.domain.GameTypes;
 import io.deeplay.camp.game.entites.Cell;
 import io.deeplay.camp.game.entites.Field;
 import io.deeplay.camp.game.entites.Move;
+import io.deeplay.camp.game.entites.Ship;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameLoggerTest {
 
     @Test
     void gameLoggerTest() {
-        GameLogger gameLogger = new GameLogger();
+        Field field = new Field(5);
+        GameLogger gameLogger = new GameLogger(field);
         String gameId = "TestID";
         GameTypes gameType = GameTypes.HumanVsBot;
         String PlayerName = "Test";
-        Field field = new Field(5);
-        Cell startPositionSM = new Cell(1, 1);
-        Cell endPositionSM = new Cell(5, 5);
-        Move move = new Move(startPositionSM, endPositionSM, Move.MoveType.ORDINARY, 5);
+        String PlayerName2 = "Test2";
 
+        Cell endPositionSM = new Cell(1, 1);
+        Cell startPositionSM = new Cell(0, 4);
+
+        Move move = new Move(startPositionSM, endPositionSM, Move.MoveType.ORDINARY, 5);
+        List<Ship.ShipType> ships = new ArrayList<>();
+        ships.add(Ship.ShipType.BASIC);
         gameLogger.startGameSession(gameId, gameType);
         gameLogger.connectingPlayer(PlayerName);
+        gameLogger.connectingPlayer(PlayerName2);
         gameLogger.gameStarted(field);
+        gameLogger.createShips(ships, PlayerName);
         gameLogger.getPlayerAction(move, PlayerName);
         gameLogger.gameEnded(PlayerName);
         gameLogger.endGameSession();
